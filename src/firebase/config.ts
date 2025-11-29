64b805ab5c5fb6b2ac203146e477053d89a47024
ArticD3v <articd3v@gmail.com>
2025-11-30 02:06:35 +0530

    config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-96464903-743a1",
-  "appId": "1:427597063597:web:1516c17029df3cfb74ed24",
-  "apiKey": "AIzaSyCc4S9VIteHBem0PBWdjKivKIJkFeZJzFA",
-  "authDomain": "studio-96464903-743a1.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "427597063597"
+  "projectId": "zorvexraxod3-43b79",
+  "appId": "1:1037588878144:web:dd641394a211a6009760a5",
+  "apiKey": "AIzaSyCBdk90XELrfVs3fAEjXSX1qIbcN55lzqI",
+  "authDomain": "zorvexraxod3-43b79.firebaseapp.com",
+  "measurementId": "G-KGP7HH4SCS",
+  "messagingSenderId": "1037588878144"
 };
