278f9c1c9fdc0d903d374fac4ad41505105f70ee
ArticD3v <articd3v@gmail.com>
2025-11-30 04:33:36 +0530

    Delete .next directory

--- .next/static/chunks/src_app_login_page_tsx_d846e3da._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_1f6d10df._.js",
-  "static/chunks/src_8d7fbc2a._.js"
-],
-    source: "dynamic"
-});
